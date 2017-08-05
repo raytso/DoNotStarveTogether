8ebc16d5ba08e5618c436070009d6675c35634bf
Ray <raytso790211@gmail.com>
2017-08-06 02:46:20 +0800

    Day 13

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,4 @@
+return {
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-503919639"]={ configuration_options={ FIX_EQUIP_NOISE=false }, enabled=true } 
+}
